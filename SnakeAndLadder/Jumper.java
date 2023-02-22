bed82b034cc9023f38ab0da0619a2b4b7840b7b1
Divya251199 <92331091+Divya251199@users.noreply.github.com>
2023-02-22 21:06:16 +0530

    Add files via upload

---
+++ SnakeAndLadder/Jumper.java
@@ -0,0 +1,20 @@
+public class Jumper {
+    String name;
+    Coordinates start;
+    Coordinates end;
+
+    Jumper(Coordinates start, Coordinates end) {
+        this.start = start;
+        this.end = end;
+        this.name = start.rn < end.rn ? "Snake" : "Ladder";
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.name).append("\n");
+        sb.append("Start -> ").append(start.toString()).append(", End -> ").append(end.toString());
+        return sb.toString();
+    }
+
+}
